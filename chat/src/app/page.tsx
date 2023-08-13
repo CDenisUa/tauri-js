1ea243ca6b17c143f1e9e2143f9286a705db5edb
Denys Chepiha <dchepiha@exoft.net>
2023-08-14 00:02:36 +0200

    Add first component

--- chat/src/app/page.tsx
+++ chat/src/app/page.tsx
@@ -1,9 +1,21 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+// Core
+import Image from 'next/image';
 
-      </div>
+const Home = () => (
+  <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <Image
+        width='48'
+        height='48'
+        className='mx-auto w-auto'
+        src='/images/logo.png'
+        alt='logo'
+      />
+      <h2 className='mt-6 text-center text-3xl font-bold tracking-tighter text-gray-900'>
+        Sign in to your account
+      </h2>
     </div>
-  );
-}
+  </div>
+)
+
+export default Home;
